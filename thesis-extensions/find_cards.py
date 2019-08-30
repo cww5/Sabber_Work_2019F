0e0392f553ff44ea0e5938b6ba960109a323ec05
Connor Watson <connorwatson0811@gmail.com>
2019-08-30 17:03:41 -0400

    8-30-19 Using argparse over sys.argv

--- thesis-extensions/find_cards.py
+++ thesis-extensions/find_cards.py
@@ -1,15 +1,24 @@
-"""This is a simple script to conduct some simple data mining. There are two possible configurations:
+desc = """This is a simple script to conduct some simple data mining. There are two possible configurations:
 
 Option 1: Create the set of English cards:
 C:|...\thesis-extensions\python test.py  ..\thesis-output\CardDefs.xml ..\thesis-output\CardsEng.txt
 
 Option 2: Search the set of English cards using a decklist:
-C:\...\thesis-extensions\python test.py ..\thesis-output\<decklist>/txt ..\thesis-output\CardsEng.txt
+C:\...\thesis-extensions\python test.py ..\thesis-output\<decklist>.txt ..\thesis-output\CardsEng.txt
 
 Both runs REQUIRE that the second argument is the path to the parsed list of English cards (which also
 contains any lines of xml with english text, since the search parameter in Option 1 is <enUS>"""
 
+import argparse
 import sys
+
+
+def get_arguments():
+	parser = argparse.ArgumentParser(description=desc)
+	parser.add_argument('infile', help='Either [CardDefs.xml, <decklist>.txt]')
+	parser.add_argument('cardseng', help='Must be path to: CardsEng.txt')
+	args = parser.parse_args()
+	return args
 
 
 def parse_cards_xml(fname, cards_eng_path):
@@ -69,17 +78,20 @@ def parse_decklist(fname, cards_eng_path):
 	deck.close()
 
 
-assert len(sys.argv) == 3, '''Please input 2 additional arguments: inFile, and secFile. \n
-inFile is path to either [CardDefs.xml, CardsEng.txt] and \n
-secFile is path to either [<decklist>.txt, CardsEng.txt]'''
+def main():
+	cmd_args = get_arguments()
 
-inFile = sys.argv[1]
-option = inFile[-3:]
-eng_cards_path = sys.argv[2]
+	infile = cmd_args.infile
+	option = infile[-3:]
+	eng_cards_path = cmd_args.cardseng
 
-if option == 'xml':
-	parse_cards_xml(inFile, eng_cards_path)
-elif option == 'txt':
-	parse_decklist(inFile, eng_cards_path)
-else:
-	print('Invalid option. argv[2] xml to parse CardDefs.xml, or eng to parse the created file with eng cards')
+	if option == 'xml':
+		parse_cards_xml(infile, eng_cards_path)
+	elif option == 'txt':
+		parse_decklist(infile, eng_cards_path)
+	else:
+		print('Invalid option. argv[2] xml to parse CardDefs.xml, or eng to parse the created file with eng cards')
+
+
+if __name__ == '__main__':
+	main()
